7f8d0397ebd9fef43fa28ae059711eec98dcd593
PandoraBox06 <axel.heros06@hotmail.fr>
2025-07-18 09:57:06 +0200

    Coeur

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,17 @@ int main()
     gl::init("Courbes Paramétriques");
     gl::maximize_window();
 
-    std::vector<glm::vec2> circle;
-    glm::vec2 center = {0.f, 0.f};
-    float radius = 1.f;
-    int segments = 256;
+    std::vector<glm::vec2> heart_points;
+    int segments = 300;
 
     for (int i = 0; i < segments; ++i)
     {
-        float t = float(i) / float(segments);
-        float angle = t * 2.f * glm::pi<float>();
-        glm::vec2 point = center + radius * glm::vec2(std::cos(angle), std::sin(angle));
-        circle.push_back(point);
+        float t = glm::pi<float>() * 2.f * i / segments;
+
+        float x = 16 * std::pow(std::sin(t), 3);
+        float y = 13 * std::cos(t) - 5 * std::cos(2 * t) - 2 * std::cos(3 * t) - std::cos(4 * t);
+
+        heart_points.emplace_back(glm::vec2{x, y} * 0.025f); // scale down
     }
 
     while (gl::window_is_open())
@@ -28,6 +28,6 @@ int main()
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        utils::draw_polyline(circle, true, 0.002f, {1.f, 1.f, 1.f, 1.f}); // cercle blanc
+        utils::draw_polyline(heart_points, true, 0.004f, {1.f, 1.f, 1.f, 1.f});
     }
 }
